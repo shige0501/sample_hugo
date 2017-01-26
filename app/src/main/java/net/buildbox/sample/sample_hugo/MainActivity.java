2560253b36105e5aae5ed239da3293ddebab9552
Koji Shigemura <shige0501@gmail.com>
2017-01-26 23:31:09 +0900

    add sample code

--- app/src/main/java/net/buildbox/sample/sample_hugo/MainActivity.java
+++ app/src/main/java/net/buildbox/sample/sample_hugo/MainActivity.java
@@ -2,6 +2,9 @@ package net.buildbox.sample.sample_hugo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import hugo.weaving.DebugLog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toast();
+    }
+
+    @DebugLog
+    public String toast() {
+        Toast.makeText(this, "サンプルメッセージ", Toast.LENGTH_SHORT).show();
+        return "表示完了";
     }
 }
